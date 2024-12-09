ff1fdee89538445b233d2640c362649c505af1b3
Charan Santhosh Gudiseva <charansanthosh1675@gmail.com>
2024-12-08 20:41:41 -0600

    Fix DATABASE_URL configuration

--- eyeopticals/settings.py
+++ eyeopticals/settings.py
@@ -62,10 +62,13 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'eyeopticals.wsgi.application'
 
-# Database Configuration
+BASE_DIR = Path(__file__).resolve().parent.parent
+
 DATABASES = {
     'default': dj_database_url.config(
-        default=os.getenv("DATABASE_URL", f'sqlite:///{BASE_DIR / "db.sqlite3"}')
+        default=os.getenv('DATABASE_URL', f'sqlite:///{BASE_DIR}/db.sqlite3'),
+        conn_max_age=600,
+        ssl_require=False
     )
 }
 
